c0d732b8cf152f2ec1d50d50c349cbd48952570c
Chanakykumar Bhavsar <email2bhavsar@users.noreply.github.com>
2020-09-20 20:12:25 -0500

    payload system 2.2
    payload system 2.2

--- __config__.py
+++
@@ -1,3 +0,0 @@
-import os
-
-user_path = os.path.join(os.getenv('HOME', ''), 'aospy_user', 'aospy_user')
--- __init__.py
+++ __init__.py
@@ -1,26 +1,16 @@
-"""aospy: management, and analysis of gridded climate data."""
-from .__config__ import user_path
-from . import _constants
-from . import utils
-from . import var
-from .var import Var
-from . import region
-from .region import Region
-from . import run
-from .run import Run
-from . import model
-from .model import Model
-from . import proj
-from .proj import Proj
-from . import calc
-from .calc import Calc
-from .automate import submit_mult_calcs
-from . import examples
+#!/usr/bin/env python3
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+# This file is part of dbxfs.
 
-__all__ = ['user_path', '_constants', 'utils', 'var', 'Var', 'region',
-           'Region', 'run', 'Run', 'model', 'Model', 'proj', 'Proj',
-           'calc', 'Calc', 'submit_mult_calcs', 'examples']
+# dbxfs is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# dbxfs is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with dbxfs.  If not, see <http://www.gnu.org/licenses/>.
